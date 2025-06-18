937d17f5a47c9ae463e2216394a9a9bbbf86978d
kashifakhan1996 <khankashifa.1996@gmail.com>
2025-06-18 15:04:20 +0530

    first commit

---
+++ src/components/ColumnContainer.tsx
@@ -0,0 +1,115 @@
+import { useSortable } from "@dnd-kit/sortable";
+import TrashIcon from "../icons/TrashIcon";
+import type { Column } from "../types";
+import { CSS } from "@dnd-kit/utilities";
+
+interface Props {
+  column: Column;
+  deleteColumn: () => void;
+}
+const ColumnContainer = (props: Props) => {
+  const { column, deleteColumn } = props;
+  const {
+    setNodeRef,
+    attributes,
+    listeners,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: column.id,
+    data: {
+      type: "Column",
+      column,
+    },
+  });
+
+  const style = {
+    transition,
+    transform: CSS.Transform.toString(transform),
+  };
+  if (isDragging) {
+    return (
+      <div
+        style={style}
+        ref={setNodeRef}
+        className="
+        flex
+        flex-col
+        bg-columnBackgroundColor
+        w-[350px]
+        h-[500px]
+        min-h-[500px]
+        opacity-40
+        border-2
+        border-rose-500
+            "
+      ></div>
+    );
+  } else {
+    return (
+      <div
+        style={style}
+        ref={setNodeRef}
+        className="
+        flex
+        flex-col
+        bg-columnBackgroundColor
+        w-[350px]
+        h-[500px]
+        min-h-[500px]
+        "
+      >
+        <div
+          {...attributes}
+          {...listeners}
+          className="
+                    bg-mainBackgroundColor
+                    text-md
+                    h-[60px]
+                    cursor-grab
+                    rounded-md
+                    p-3
+                    font-bold
+                    border-columnBackgroundColor
+                    border-4
+                    flex
+                    items-center
+                    justify-between
+                "
+        >
+          <div className="flex gap-2">
+            <div
+              className="
+                            flex
+                            justify-center
+                            items-center
+                            bg-columnBackgroundColor
+                            px-2
+                            py-1
+                            text-sm
+                            rounded-full
+                        "
+            >
+              0
+            </div>
+            {column && column.title}
+          </div>
+          <button onClick={deleteColumn}>
+            <TrashIcon
+              extClass={`
+                        rounded
+                        stroke-gray-500
+                        hover:stroke-white
+                        hover:bg-columnBackgroundColor`}
+            />
+          </button>
+        </div>
+
+        <div className="flex flex-grow">Content here</div>
+      </div>
+    );
+  }
+};
+
+export default ColumnContainer;
---
+++ src/components/KanbanBoard.tsx
@@ -0,0 +1,124 @@
+import { useMemo, useState } from "react";
+import { PlusIcon } from "../icons/PlusIcon";
+import type { Column, Id } from "../types";
+import ColumnContainer from "./ColumnContainer";
+import {
+  DndContext,
+  DragOverlay,
+  type DragEndEvent,
+  type DragStartEvent,
+} from "@dnd-kit/core";
+import { arrayMove, SortableContext } from "@dnd-kit/sortable";
+import { createPortal } from "react-dom";
+
+const KanbanBoard = () => {
+  const [columns, setColumn] = useState<Column[]>([]);
+  const [activeColumn, setActiveColumn] = useState<Column | null>();
+
+  const columnsId = useMemo(() => columns.map((col) => col.id), [columns]);
+  const addNewColumn = () => {
+    const columnToAdd: Column = {
+      id: generateId(),
+      title: `Column ${columns.length + 1}`,
+    };
+    setColumn((prev) => [...prev, columnToAdd]);
+    console.log(columns);
+  };
+  const generateId = () => {
+    const randomId = Math.floor(Math.random() * 1000);
+    return randomId;
+  };
+  const deleteColumn = (id: Id) => {
+    setColumn(columns.filter((col) => col.id !== id));
+  };
+  const ondragstart = (event: DragStartEvent) => {
+    if (event.active.data.current?.type === "Column") {
+      setActiveColumn(event.active.data.current.column);
+    }
+  };
+  const ondragend = (event: DragEndEvent) => {
+    const { over, active } = event;
+    if (!over) {
+      return;
+    }
+    const overColumnId = over.id;
+    const activeColumnId = active.id;
+    if (overColumnId === activeColumnId) {
+      return;
+    }
+
+    setColumn((columns) => {
+      const activeColumnIndex = columns.findIndex(
+        (col) => col.id === activeColumnId
+      );
+      const overColumnIndex = columns.findIndex(
+        (col) => col.id === overColumnId
+      );
+      return arrayMove(columns, activeColumnIndex, overColumnIndex);
+    });
+  };
+
+  return (
+    <div
+      className="
+    m-auto
+    flex
+    min-h-screen
+    w-full
+    items-center
+    overflow-x-auto
+    overflow-y-hidden
+    px-[40px]
+    "
+    >
+      <DndContext onDragStart={ondragstart} onDragEnd={ondragend}>
+        <div className="m-auto flex gap-4">
+          <div className="flex gap-4">
+            <SortableContext items={columnsId}>
+              {columns.map((col, idx) => (
+                <ColumnContainer
+                  column={col}
+                  key={idx}
+                  deleteColumn={() => deleteColumn(col.id)}
+                />
+              ))}
+            </SortableContext>
+          </div>
+          <button
+            onClick={() => addNewColumn()}
+            className="
+            h-[60px]
+            w-[350px]
+            min-w-[350px]
+            cursor-pointer
+            rounded-lg
+            bg-mainBackgroundColor
+            border-2
+            border-columnBackgroundColor
+            ring-rose-500
+            hover:ring-2
+            flex
+            gap-2
+            p-4
+            "
+          >
+            <PlusIcon /> Add Column
+          </button>
+        </div>
+        {createPortal(
+          <DragOverlay>
+            {activeColumn && (
+              <ColumnContainer
+                column={activeColumn}
+                deleteColumn={() => deleteColumn(activeColumn.id)}
+              />
+            )}
+          </DragOverlay>,
+          document.body
+        )}
+      </DndContext>
+    </div>
+  );
+};
+
+export default KanbanBoard;
